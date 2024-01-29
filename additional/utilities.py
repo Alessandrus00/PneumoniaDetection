11e249b53fe4e36424073e3617b5d2f09cd2692e
Alessandro Resta <77611890+Alessandrus00@users.noreply.github.com>
2024-01-29 22:46:45 +0100

    anche l'occhio vuole la sua parte

--- additional/utilities.py
+++ additional/utilities.py
@@ -1,19 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import random
 
 def plot_results(n_epochs, train_losses, train_accs, valid_losses, valid_accs):
   N_EPOCHS = n_epochs
   # Plot results
   plt.figure(figsize=(20, 6))
   _ = plt.subplot(1,2,1)
-  plt.plot(np.arange(N_EPOCHS) + 1, train_losses, linewidth=3)
-  plt.plot(np.arange(N_EPOCHS) + 1, valid_losses, linewidth=3)
+  plt.plot(np.arange(N_EPOCHS) + 1, train_losses, 'o-', linewidth=3)
+  plt.plot(np.arange(N_EPOCHS) + 1, valid_losses, 'o-', linewidth=3)
   _ = plt.legend(['Train', 'Validation'])
   plt.grid('on'), plt.xlabel('Epoch'), plt.ylabel('Loss')
 
   _ = plt.subplot(1,2,2)
-  plt.plot(np.arange(N_EPOCHS)+1, train_accs, linewidth=3)
-  plt.plot(np.arange(N_EPOCHS)+1, valid_accs, linewidth=3)
+  plt.plot(np.arange(N_EPOCHS)+1, train_accs, 'o-', linewidth=3)
+  plt.plot(np.arange(N_EPOCHS)+1, valid_accs, 'o-', linewidth=3)
   _ = plt.legend(['Train', 'Validation'])
   plt.grid('on'), plt.xlabel('Epoch'), plt.ylabel('Accuracy')
 
--- models/model_functions.py
+++ models/model_functions.py
@@ -1,4 +1,5 @@
 import torch
+from tqdm.auto import tqdm, trange
 
 def model_training(n_epochs, model, train_loader, val_loader, optimizer, criterion, device):
   train_losses = []
@@ -9,12 +10,14 @@ def model_training(n_epochs, model, train_loader, val_loader, optimizer, criteri
   best_epoch = 0
 
   for epoch in range(1, n_epochs + 1):
+    train_progress_bar = tqdm(train_loader, desc=f"Training epoch {epoch}/{n_epochs}", leave=False, unit="mini-batch")
+    train_acc, train_loss = train(model, train_loader, optimizer, criterion, device, train_progress_bar)
+    print(f'Epoch: {epoch:03d}')
+    print(f'\tTrain Loss: {train_loss:.4f}, Train Accuracy: {train_acc:.2f}%')
 
-    train_acc, train_loss = train(model, train_loader, optimizer, criterion, device)
-    val_acc, val_loss = eval(model, val_loader, criterion, device)
-
-    print(f'Epoch: {epoch:03d}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}')
-    print(f'Epoch: {epoch:03d}, Train Accuracy: {train_acc:.4f}%, Val Accuracy: {val_acc:.4f}%')
+    val_progress_bar = tqdm(val_loader, desc=f"Validation epoch {epoch}/{n_epochs}", leave=False, unit="mini-batch")
+    _, _, val_acc, val_loss = eval(model, val_loader, criterion, device, val_progress_bar)
+    print(f'\tVal Loss: {val_loss:.4f}, Val Accuracy: {val_acc:.2f}%')
 
     if val_acc > best_val_acc:
         best_val_acc = val_acc
@@ -29,14 +32,14 @@ def model_training(n_epochs, model, train_loader, val_loader, optimizer, criteri
   return train_losses, train_accs, val_losses, val_accs, best_epoch
 
 
-def train(model, data_loader, optimizer, criterion, device):
+def train(model, data_loader, optimizer, criterion, device, progress_bar):
     total = 0.
     correct = 0.
     total_loss = 0.
 
     model.train()
 
-    for images, labels in data_loader:
+    for images, labels in progress_bar:
           images = images.to(device)
           labels = labels.to(device)
 
@@ -57,16 +60,20 @@ def train(model, data_loader, optimizer, criterion, device):
 
     return float(correct * 100. / total), total_loss / len(data_loader)
 
-def eval(model, data_loader, criterion, device):
+def eval(model, data_loader, criterion, device, progress_bar=None):
     total = 0.
     correct = 0.
     total_loss = 0.
+    true_labels = []
+    predicted_labels = []
+
+    if progress_bar is None:
+       progress_bar = tqdm(data_loader, desc=f"Test", leave=False, unit="mini-batch")
 
     model.eval()
-
     with torch.no_grad():
 
-        for images, labels in data_loader:
+        for images, labels in progress_bar:
 
             images = images.to(device)
             labels = labels.to(device)
@@ -80,7 +87,11 @@ def eval(model, data_loader, criterion, device):
             _, pred = torch.max(out, dim=1)
             correct += torch.sum(pred == labels)
 
-    return float(correct * 100. / total), total_loss / len(data_loader)
+            # for test metrics
+            true_labels.extend(labels.cpu().numpy())
+            predicted_labels.extend(pred.cpu().numpy())
+
+    return true_labels, predicted_labels, float(correct * 100. / total), total_loss / len(data_loader)
 
 # This function can be made more general by allowing for more flexible layer freezing options.
 def freeze(model, classification_layer):
