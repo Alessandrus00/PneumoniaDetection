1e50c1ad253e0be2a06bc3e28d88eac47835153e
Alessandro Resta <77611890+Alessandrus00@users.noreply.github.com>
2024-01-29 20:48:05 +0100

    add train val split

--- additional/utilities.py
+++ additional/utilities.py
@@ -16,3 +16,34 @@ def plot_results(n_epochs, train_losses, train_accs, valid_losses, valid_accs):
   plt.plot(np.arange(N_EPOCHS)+1, valid_accs, linewidth=3)
   _ = plt.legend(['Train', 'Validation'])
   plt.grid('on'), plt.xlabel('Epoch'), plt.ylabel('Accuracy')
+
+
+def extract_patient_ids(filename):
+    patient_id = filename.split('_')[0].replace("person", "")
+    return patient_id
+
+
+def split_file_names(input_folder, val_split_perc):
+    # Pneumonia files contain patient id, so we group split them by patient to avoid data leakage
+    pneumonia_patient_ids = set([extract_patient_ids(fn) for fn in os.listdir(os.path.join(input_folder, 'PNEUMONIA'))])
+    pneumonia_val_patient_ids = random.sample(list(pneumonia_patient_ids), int(val_split_perc * len(pneumonia_patient_ids)))
+
+    pneumonia_val_filenames = []
+    pneumonia_train_filenames = []
+
+    for filename in os.listdir(os.path.join(input_folder, 'PNEUMONIA')):
+        patient_id = extract_patient_ids(filename)
+        if patient_id in pneumonia_val_patient_ids:
+            pneumonia_val_filenames.append(os.path.join(input_folder, 'PNEUMONIA', filename))
+        else:
+            pneumonia_train_filenames.append(os.path.join(input_folder, 'PNEUMONIA', filename))
+
+    # Normal (by file, no patient information in file names)
+    normal_filenames  = [os.path.join(input_folder, 'NORMAL', fn) for fn in os.listdir(os.path.join(input_folder, 'NORMAL'))]
+    normal_val_filenames = random.sample(normal_filenames, int(val_split_perc * len(normal_filenames)))
+    normal_train_filenames = list(set(normal_filenames)-set(normal_val_filenames))
+
+    train_filenames = pneumonia_train_filenames + normal_train_filenames
+    val_filenames = pneumonia_val_filenames + normal_val_filenames
+
+    return train_filenames, val_filenames
