84b8be29b430726ab4d2c82660f9441cafe34215
Nikola Bulat <nbulat99@gmail.com>
2024-01-29 15:48:24 +0100

    Add the results plotting function

---
+++ additional/utilities.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_results(n_epochs, train_losses, train_accs, valid_losses, valid_accs):
+  N_EPOCHS = n_epochs
+  # Plot results
+  plt.figure(figsize=(20, 6))
+  _ = plt.subplot(1,2,1)
+  plt.plot(np.arange(N_EPOCHS) + 1, train_losses, linewidth=3)
+  plt.plot(np.arange(N_EPOCHS) + 1, valid_losses, linewidth=3)
+  _ = plt.legend(['Train', 'Validation'])
+  plt.grid('on'), plt.xlabel('Epoch'), plt.ylabel('Loss')
+
+  _ = plt.subplot(1,2,2)
+  plt.plot(np.arange(N_EPOCHS)+1, train_accs, linewidth=3)
+  plt.plot(np.arange(N_EPOCHS)+1, valid_accs, linewidth=3)
+  _ = plt.legend(['Train', 'Validation'])
+  plt.grid('on'), plt.xlabel('Epoch'), plt.ylabel('Accuracy')
